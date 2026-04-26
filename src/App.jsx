7579250adadeddaa6e210ff1e351f9ee9f9ad392
huzaifa gohar <hafizhuzaifa444@gmail.com>
2026-04-26 14:38:25 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Flex } from "@chakra-ui/react"; 
 import { ColorModeButton,} from "./components/ui/color-mode"; 
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; 
 import Header  from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage"; 
